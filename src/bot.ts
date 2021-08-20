e19ff9cbe97c37f6d3e773c77afdb1f012b3a63f
Tan Zon <tanhoang.cntt@gmail.com>
2021-08-20 11:39:55 +0700

    update

--- src/bot.ts
+++ src/bot.ts
@@ -1,12 +1,19 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
 import * as ethers from 'ethers'
 import * as chalk from 'chalk'
-import fs from 'fs'
+import { TOKEN_SNIPERS } from './config'
+
+enum CRYPTO_TYPE {
+    BNB,
+    BUSD,
+}
 
 @Injectable()
 export class Bot implements OnModuleInit {
     private config = {
         WBNB: process.env.WBNB_CONTRACT, // wbnb
+
+        BUSD: process.env.BUSD_CONTRACT, // wbnb
 
         TO_PURCHASE_ADDRESS: process.env.TO_PURCHASE, // token that you will purchase = BUSD for test '0xe9e7cea3dedca5984780bafc599bd69add087d56'
 
@@ -24,6 +31,10 @@ export class Bot implements OnModuleInit {
 
         MAX_LIQUIDITY_ADDED: process.env.MAX_LIQUIDITY_ADDED, // max liquidity added
 
+        MIN_FIXED_BNB_LIQUIDITY_ADDED: process.env.MIN_FIXED_BNB_LIQUIDITY_ADDED, // min fixed liquidity added
+
+        MIN_FIXED_BUSD_LIQUIDITY_ADDED: process.env.MIN_FIXED_BUSD_LIQUIDITY_ADDED, // min fixed liquidity added
+
         WSS_NODE: process.env.WSS_NODE,
 
         RPC_NODE: process.env.RPC_NODE,
@@ -33,14 +44,10 @@ export class Bot implements OnModuleInit {
         MIN_BALANCE: process.env.MIN_BALANCE,
     }
 
-    private initialLiquidityDetected = false
+    private tokensAlreadyToBuy = []
     private canBuy = true
-    private jmlBnb = ''
     private balanceAvailable = true
     private isFixedToken = true
-
-    private tokenIn = this.config.WBNB
-    private tokenOut = this.config.TO_PURCHASE_ADDRESS
 
     private provider
     private wallet
@@ -48,11 +55,12 @@ export class Bot implements OnModuleInit {
     private factory
     private router
     private wbnb
+    private busd
 
     async init() {
         try {
-            this.provider = new ethers.providers.JsonRpcProvider(this.config.RPC_NODE)
-            // this.provider = new ethers.providers.WebSocketProvider(this.config.WSS_NODE)
+            // this.provider = new ethers.providers.JsonRpcProvider(this.config.RPC_NODE)
+            this.provider = new ethers.providers.WebSocketProvider(this.config.WSS_NODE)
             this.wallet = ethers.Wallet.fromMnemonic(this.config.MY_MNEMONIC)
             this.account = this.wallet.connect(this.provider)
 
@@ -73,14 +81,23 @@ export class Bot implements OnModuleInit {
                     'function swapExactTokensForTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
                     // eslint-disable-next-line max-len
                     'function swapExactTokensForTokensSupportingFeeOnTransferTokens(uint amountIn, uint amountOutMin, address[] calldata path, address to, uint deadline) external returns (uint[] memory amounts)',
-                    // eslint-disable-next-line max-len
-                    'function swapExactETHForTokens(uint amountOutMin, address[] calldata path, address to, uint deadline) external payable returns (uint[] memory amounts)',
+                    'function swapExactETHForTokensSupportingFeeOnTransferTokens(uint256 amountOutMin, address[] path, address to, uint256 deadline)',
+                    'function swapExactETHForTokens(uint256 amountOutMin, address[] path, address to, uint256 deadline)',
                 ],
                 this.account,
             )
 
             this.wbnb = new ethers.Contract(
                 this.config.WBNB,
+                [
+                    'function approve(address spender, uint amount) public returns(bool)',
+                    'function balanceOf(address account) external view returns (uint256)',
+                ],
+                this.account,
+            )
+
+            this.busd = new ethers.Contract(
+                this.config.BUSD,
                 [
                     'function approve(address spender, uint amount) public returns(bool)',
                     'function balanceOf(address account) external view returns (uint256)',
@@ -96,29 +113,33 @@ export class Bot implements OnModuleInit {
         await this.init()
         console.log('------Bot Running------')
         // await this.checkLiquid()
-        await this.approved()
-        const address = await this.account.getAddress()
-        const balance = await this.wbnb.balanceOf(address.toString())
-        const balanceValue = ethers.utils.formatEther(balance)
-        if (balanceValue <= this.config.MIN_BALANCE) {
-            this.balanceAvailable = false
-        }
+        // await Promise.all([
+        //     this.approved(),
+            // this.approvedBusd(),
+        // ])
+        // const address = await this.account.getAddress()
+        // const balance = await this.wbnb.balanceOf(address.toString())
+        // const balanceValue = ethers.utils.formatEther(balance)
 
-        this.factory.on('PairCreated', async (token0, token1, pairAddress) => {
-            const tokenAddress = token0.toUpperCase() === this.config.WBNB.toUpperCase() ? token1 : token0
-            console.log(`
-            New pair detected
-            =================
-            tokenAddress: ${tokenAddress}
-            pairAddress: ${pairAddress}
-          `)
-            if (this.balanceAvailable) {
-                if (this.isFixedToken) {
-                    if (tokenAddress.toUpperCase() === this.config.TO_PURCHASE_ADDRESS.toUpperCase()) {
-                        console.log(`Token need to purchase: ${this.config.TO_PURCHASE_ADDRESS}`)
-                        setTimeout(() => this.buyAction(), 1)
-                    }
-                } else {
+        // if (balanceValue <= this.config.MIN_BALANCE) {
+        //     this.balanceAvailable = false
+        // }
+
+        if (this.balanceAvailable) {
+            if (this.isFixedToken) {
+                for (const [k, v] of Object.entries(TOKEN_SNIPERS)) {
+                    await this.checkLiquid(k, v)
+                }
+            } else {
+                this.factory.on('PairCreated', async (token0, token1, pairAddress) => {
+                    const tokenAddress = token0.toUpperCase() === this.config.WBNB.toUpperCase() ? token1 : token0
+                    console.log(`
+                        New pair detected
+                        =================
+                        tokenAddress: ${tokenAddress}
+                        pairAddress: ${pairAddress}`
+                    )
+                    
                     const pairBNBValue = await this.wbnb.balanceOf(pairAddress)
                     const fmValue: any = ethers.utils.formatEther(pairBNBValue)
                     console.log(`${chalk.cyan(`---Liquidity pool value : ${fmValue}---`)}`)
@@ -128,26 +149,38 @@ export class Bot implements OnModuleInit {
                         if (this.canBuy) {
                             // buy once time
                             this.canBuy = false
-                            console.log(`Balance available: ${balanceValue}`)
-                            setTimeout(() => this.buy(tokenAddress), 1)
+                            setTimeout(() => this.buy(this.config.WBNB, tokenAddress, this.config.AMOUNT_OF_WBNB), 1)
                         }
                     } else {
                         console.log(`${chalk.yellow(`Next pair >>>>>>>>>>>>>>>>>>>>>`)}`)
                     }
-                }
+                })
             }
-        })
+        } else {
+            console.log(`${chalk.red(`.............Your balance not available to buy.............`)}`)
+        }
     }
 
-    async buy(tokenAddress) {
+    async buy(tokenAddress, amount, type) {
         try {
-            console.log(`----Ready to buy----`)
+            if (this.tokensAlreadyToBuy.includes(tokenAddress)) {
+                console.log(chalk.green(`Token buy already in progress.............`))
+                return
+            }
+            this.tokensAlreadyToBuy.push(tokenAddress)
 
+            let tokenIn
+            if (type === CRYPTO_TYPE.BNB) {
+                tokenIn = this.config.WBNB
+            } else {
+                tokenIn = this.config.BUSD
+            }
+            console.log(`----Ready to buy----`)
             let amountOutMin = 0
             // We buy x amount of the new token for our wbnb
-            const amountIn: any = ethers.utils.parseUnits(`${this.config.AMOUNT_OF_WBNB}`, 'ether')
+            const amountIn: any = ethers.utils.parseUnits(`${amount}`, 'ether')
             if (parseInt(this.config.slippage, 10) !== 0) {
-                const amounts = await this.router.getAmountsOut(amountIn, [this.tokenIn, tokenAddress])
+                const amounts = await this.router.getAmountsOut(amountIn, [tokenIn, tokenAddress])
                 // Our execution price will be a bit different, we need some flexbility
                 amountOutMin = amounts[1].sub(amounts[1].div(`${this.config.slippage}`))
             }
@@ -155,24 +188,22 @@ export class Bot implements OnModuleInit {
             console.log(
                 `${chalk.green(`Start to buy \n`)}
                 Buying Token=================
-                tokenIn: ${(amountIn * 1e-18).toString()} ${this.tokenIn} (BNB)
-                tokenOut: ${amountOutMin.toString()} ${tokenAddress}
-              `,
+                tokenIn: ${(amountIn * 1e-18).toString()} ${tokenIn}
+                tokenOut: ${amountOutMin.toString()} ${tokenAddress}`,
             )
 
             console.log('Processing Transaction.....')
-            console.log(chalk.green(`amountIn: ${(amountIn * 1e-18).toString()} ${this.tokenIn} (BNB)`))
+            console.log(chalk.green(`amountIn: ${(amountIn * 1e-18).toString()} ${tokenIn}`))
             console.log(chalk.green(`amountOutMin: ${amountOutMin}`))
-            console.log(chalk.green(`tokenIn: ${this.tokenIn}`))
+            console.log(chalk.green(`tokenIn: ${tokenIn}`))
             console.log(chalk.green(`tokenOut: ${tokenAddress}`))
             console.log(chalk.green(`your wallet: ${this.config.recipient}`))
 
             // eslint-disable-next-line max-len
             // const tx = await router.swapExactTokensForTokensSupportingFeeOnTransferTokens( //uncomment this if you want to buy deflationary token
-            const tx = await this.router.swapExactTokensForTokens( // uncomment here if you want to buy token
-                amountIn,
-                amountOutMin,
-                [this.tokenIn, tokenAddress],
+            const tx = await this.router.swapExactETHForTokens( // uncomment here if you want to buy token
+                0,
+                [tokenIn, tokenAddress],
                 this.config.recipient,
                 Date.now() + 1000 * 60 * 10, // 10 minutes
                 {
@@ -184,13 +215,6 @@ export class Bot implements OnModuleInit {
             const receipt = await tx.wait()
             console.log(`---Transaction receipt---`)
             console.log(`https://www.bscscan.com/tx/${receipt.transactionHash}`)
-            fs.appendFile('token_bought.txt', `${tokenAddress}\n`, 'utf8',
-                // callback function
-                (err) => {
-                    if (err) throw err
-                    // if no error
-                    console.log('Token is appended to file successfully.')
-                })
         } catch (err) {
             console.log(`----Buy error: ${err}----`)
         }
@@ -207,78 +231,74 @@ export class Bot implements OnModuleInit {
         console.log(`https://www.bscscan.com/tx/${receipt.transactionHash}`)
     }
 
-    async checkLiquid() {
-        try {
-            const pairAddress = await this.factory.getPair(this.tokenIn, this.tokenOut)
-            console.log(chalk.blue(`---PairAddress: ${pairAddress}---`))
-            if (pairAddress !== null && pairAddress !== undefined) {
-                if (pairAddress.toString().indexOf('0x0000000000000') > -1) {
-                    console.log(chalk.cyan(`---PairAddress ${pairAddress} not detected. Auto restart---`))
-                    await this.checkLiquid()
-                }
-            }
-            const pairBNBValue = await this.wbnb.balanceOf(pairAddress)
-            this.jmlBnb = ethers.utils.formatEther(pairBNBValue)
-            console.log(`---Value BNB : ${this.jmlBnb}---`)
+    async approvedBusd() {
+        const tx = await this.busd.approve(
+            this.router.address,
+            ethers.constants.MaxUint256,
+            {
+                gasLimit: 100000,
+            },
+        )
 
-            if (this.jmlBnb > this.config.MIN_LIQUIDITY_ADDED) {
-                setTimeout(() => this.buyAction(), 10)
-            } else {
-                this.initialLiquidityDetected = false
-                console.log('---Run again---')
-                await this.checkLiquid()
+        const receipt = await tx.wait()
+        console.log('---Approve transaction receipt---')
+        console.log(`https://www.bscscan.com/tx/${receipt.transactionHash}`)
+    }
+
+    async checkLiquid(tokenOut, amount) {
+        try {
+            console.log(chalk.yellowBright(`Token snip: ${tokenOut}`))
+            const [pairBNBAddress, pairBUSDAdress] = await Promise.all([
+                this.factory.getPair(this.config.WBNB, tokenOut),
+                this.factory.getPair(this.config.BUSD, tokenOut),
+            ])
+
+            console.log(chalk.green(`Pair bnb: ${pairBNBAddress}`))
+            console.log(chalk.green(`Pair busd: ${pairBUSDAdress}`))
+
+            if (pairBNBAddress) {
+                setTimeout(() => this.processPairAddress(tokenOut, pairBNBAddress, amount, CRYPTO_TYPE.BNB), 1)
+            } else if (pairBUSDAdress) {
+                setTimeout(() => this.processPairAddress(tokenOut, pairBUSDAdress, amount, CRYPTO_TYPE.BUSD), 1)
             }
         } catch (err) {
             console.log(err)
         }
     }
 
-    async buyAction() {
-        console.log(`${chalk.green(`---Ready to buy---`)}`)
+    async processPairAddress(tokenOut, pairAddress, amount, type) {
         try {
-            let amountOutMin = 0
-            // We buy x amount of the new token for our wbnb
-            const amountIn: any = ethers.utils.parseUnits(`${this.config.AMOUNT_OF_WBNB}`, 'ether')
-            if (parseInt(this.config.slippage, 10) !== 0) {
-                const amounts = await this.router.getAmountsOut(amountIn, [this.tokenIn, this.tokenOut])
-                // Our execution price will be a bit different, we need some flexbility
-                amountOutMin = amounts[1].sub(amounts[1].div(`${this.config.slippage}`))
+            if (pairAddress.toString().indexOf('0x0000000000000') > -1) {
+                console.log(chalk.red(`---PairAddress ${pairAddress} not detected. Auto restart---`))
+                setTimeout(() => this.checkLiquid(tokenOut, amount), 100)
+            } else {
+                let wallet, minLiquid, amountValue
+                if (type === CRYPTO_TYPE.BNB) {
+                    wallet = this.wbnb
+                    minLiquid = this.config.MIN_FIXED_BNB_LIQUIDITY_ADDED
+                    amountValue = amount.BNB
+                } else {
+                    wallet = this.busd
+                    minLiquid = this.config.MIN_FIXED_BUSD_LIQUIDITY_ADDED
+                    amountValue = amount.BUSD
+                }
+                const liquidValue = await wallet.balanceOf(pairAddress)
+                const liquidValueF: any = ethers.utils.formatEther(liquidValue)
+                console.log(chalk.bold(chalk.red(`Pair Value: ${liquidValueF}`)))
+
+                if (liquidValueF * 1 > parseInt(minLiquid, 10)) {
+
+                    if (type === CRYPTO_TYPE.BNB) {
+                        setTimeout(() => this.buy(tokenOut, amountValue, type), 1)
+                    } else {
+                        setTimeout(() => this.buy(tokenOut, amountValue, type), 1)
+                    }
+                } else {
+                    setTimeout(() => this.checkLiquid(tokenOut, amount), 100)
+                }
             }
-
-            console.log(
-                `${chalk.green(`Start to buy \n`)}
-                Buying Token=================
-                tokenIn: ${(amountIn * 1e-18).toString()} ${this.tokenIn} (BNB)
-                tokenOut: ${amountOutMin.toString()} ${this.tokenOut}
-              `,
-            )
-
-            console.log('Processing Transaction.....')
-            console.log(chalk.yellow(`amountIn: ${(amountIn * 1e-18)} ${this.tokenIn} (BNB)`))
-            console.log(chalk.yellow(`amountOutMin: ${amountOutMin}`))
-            console.log(chalk.yellow(`tokenIn: ${this.tokenIn}`))
-            console.log(chalk.yellow(`tokenOut: ${this.tokenOut}`))
-            console.log(chalk.yellow(`your wallet: ${this.config.recipient}`))
-
-            // const tx = await router.swapExactTokensForTokensSupportingFeeOnTransferTokens( //uncomment this if you want to buy deflationary token
-            const tx = await this.router.swapExactTokensForTokens( // uncomment here if you want to buy token
-                amountIn,
-                amountOutMin,
-                [this.tokenIn, this.tokenOut],
-                this.config.recipient,
-                Date.now() + 1000 * 60 * 10, // 10 minutes
-                {
-                    gasLimit: process.env.GAS_LIMIT,
-                    gasPrice: ethers.utils.parseUnits(process.env.GWEI, 'gwei'),
-                },
-            )
-
-            const receipt = await tx.wait()
-            console.log(`---Transaction receipt---`)
-            console.log(`https://www.bscscan.com/tx/${receipt.transactionHash}`)
-            setTimeout(() => { process.exit() }, 2000)
-        } catch (err) {
-            console.log(err)
+        } catch (e) {
+            console.log(e)
         }
     }
 }
---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const TOKEN_SNIPERS = {
+    '0xaF6Bd11A6F8f9c44b9D18f5FA116E403db599f8E':{
+        BNB: 0.5,
+        BUSB: 200,
+    },
+}
